e70a35801d35a568c8c6694de5412aeb8a0fdd90
kibet kilel <amos.rotich@strathmore.edu>
2021-08-22 18:22:11 +0300

    update

--- dict/settings.py
+++ dict/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-l)t*$j42yzrt8m)!(xt24uma_t4o(98zh%vi&6yj5ft2-62r49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 # Create your views here.
+#from pyDictionary import pyDictionary
+from PyDictionary import PyDictionary
 
 def index(request):
     return render(request, 'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonym = dictionary.synonym(search)
+    antonym = dictionary.antonym(search)
+
+    context = {
+        'meaning': meaning,
+        'synonym': synonym,
+        'antonym': antonym,
+    }
+
+    return render(request, 'word.html', context)
